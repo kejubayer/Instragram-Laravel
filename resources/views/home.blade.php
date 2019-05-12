949ae1292993eaf5b62ecd4cdfbb976f06ffaad0
Eftakhar Jubayer <khondokereftakharjubayer@gmail.com>
2019-05-12 12:44:45 +0600

    profile designed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,32 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fdac24-1.fna.fbcdn.net/vp/3d1541d0ad55f0a0996595d0d868fff4/5D6975A0/t51.2885-19/s150x150/54229038_1574811422663452_7015843741034348544_n.jpg?_nc_ht=instagram.fdac24-1.fna.fbcdn.net" alt="Profile photo" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>khondoker_eftakhar_jubayer</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>166</strong>posts</div>
+                <div class="pr-5"><strong>196</strong>followers</div>
+                <div class="pr-5"><strong>461</strong>following</div>
             </div>
+            <div class="pt-4 font-weight-bold" >Khondoker Eftakhar Jubayer</div>
+            <div>I Love Coding .</div>
+            <div><a href="#">https://github.com/kejubayer</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg" alt="" class="w-100">
         </div>
     </div>
 </div>
